8962f5755a053f0d1e111307f9478c8ce9353b09
douglasmonteles <silvadouglas328@gmail.com>
2022-12-05 23:21:37 -0300

    Add readme

--- inc/I_dll.h
+++
@@ -1,7 +0,0 @@
-void init_I_dll();
-void send_msg_to_dll(char* msg, int size);
-void obtain_msg_from_dll(char* msg, int* size);
-void destroy_I_dll();
-void obtain_msg_from_dll(char *msg, int * size);
-void send_msg_to_instance(char *msg, int size);
-void obtain_msg_from_instance(char *msg, int *size);
